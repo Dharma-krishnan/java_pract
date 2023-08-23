de61a71a9e6ab1ecdd7729e49fa873483c63b728
Dharma-krishnan <dharmab2001@gmail.com>
2023-08-23 19:10:04 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,24 @@
+// import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class hello
+{
+    public static void main(String[] args) 
+    {
+      
+      int x=10;
+      int y=88;
+      int z=12;
+
+      if(x>y && x>z){
+        System.out.println("x is greater");
+      }
+      else if(y>x && y>z){
+        System.out.println("y is greater");
+      }
+      else 
+         System.out.println("z is greater");
+      
+    }
+}
