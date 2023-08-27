2e8ca72099f8ca00f8584e9d1f4c837931b974a9
Dharma-krishnan <dharmab2001@gmail.com>
2023-08-27 19:33:50 +0530

    working

--- primitives.java
+++ primitives.java
@@ -1,5 +1,10 @@
 public class primitives{
     public static void main(String[] args){
         System.out.println("hello world");
+        int rollno =10;
+        char letter='r';
+        
+
+
     }
 }
