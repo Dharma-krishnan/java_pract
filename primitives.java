02e0433b1eef2a3697b1eab22e0d23414973c4fa
Dharma-krishnan <dharmab2001@gmail.com>
2023-08-27 19:39:38 +0530

    datatypes

--- primitives.java
+++ primitives.java
@@ -3,7 +3,11 @@ public class primitives{
         System.out.println("hello world");
         int rollno =10;
         char letter='r';
-        
+        long longintvalues=1346712987;
+        float deci_vals= 3.14f;
+        double long_deci_vals= 34269.9023709;
+        boolean is_it_you=false;
+         
 
 
     }
