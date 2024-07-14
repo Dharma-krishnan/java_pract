b6a5a9a281e40bc1bbb57c0ffea42d31f447983e
Dharma-Krishnan <dharmab2001@gmail.com>
2024-07-14 15:03:52 +0530

    ops

--- src/operators.java
+++ src/operators.java
@@ -1,16 +1,22 @@
 public class operators {
     public static void main(String[] args){
 // Artimetic
-        System.out.println(4+2);
-        System.out.println(8-4);
-        System.out.println(20/2);
-        System.out.println(4%2);
-//unary operator
+//        System.out.println(4+2);
+//        System.out.println(8-4);
+//        System.out.println(20/2);
+//        System.out.println(4%2);
+////unary operator
         int a=10, b=20;
-        System.out.println(++a);
-        System.out.println(a++);
+//        System.out.println(++a);
+//        System.out.println(a++);
 
-        // assignment operator
+        // relational operators
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
 
     }
 }
